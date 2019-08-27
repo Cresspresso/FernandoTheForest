0b80500e885fceea48af04aed70b69466a30d012
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-08-28 09:36:25 +1200

    terrain textures

--- FernandoTheForest/Assets/Scripts/Player.cs
+++ FernandoTheForest/Assets/Scripts/Player.cs
@@ -6,9 +6,19 @@ public class Player : MonoBehaviour {
 
 	public CharacterController cc { get { return GetComponent<CharacterController>(); } }
 	public float speed = 10;
+	public Vector3 eulerAngles = Vector3.zero;
+
+	public Transform head;
+
+	private void Update()
+	{
+		
+	}
 
 	private void FixedUpdate()
 	{
 		cc.SimpleMove(speed * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+		head.transform.localEulerAngles = new Vector3(eulerAngles.x, 0, 0);
+		transform.localEulerAngles = new Vector3(0, eulerAngles.y, 0);
 	}
 }
