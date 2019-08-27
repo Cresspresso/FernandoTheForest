b7f5e4d2e8bfa347f23cd68e6122f58a0c4db35c
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-08-28 09:27:38 +1200

    basic stuff

---
+++ FernandoTheForest/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public CharacterController cc { get { return GetComponent<CharacterController>(); } }
+	public float speed = 10;
+
+	private void FixedUpdate()
+	{
+		cc.SimpleMove(speed * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+	}
+}
