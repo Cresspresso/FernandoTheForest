f547769bbb4b64ee0d952e3ab477c5d4283d7999
CSymphony <32253069+CSymphony@users.noreply.github.com>
2019-08-28 15:36:52 +1200

    Four Player Scores

--- FernandoTheForest/Assets/Scripts/Door.cs
+++ FernandoTheForest/Assets/Scripts/Door.cs
@@ -15,13 +15,13 @@ public class Door : MonoBehaviour {
 	public void Unlock()
 	{
 		isLocked = false;
-		UpdateAnim();
+        m_Game_Loop = GameObject.FindObjectOfType(typeof(Game_Loop)) as Game_Loop;
+        m_Game_Loop.TimetoPoints();
+        UpdateAnim();
 	}
 
 	public void UpdateAnim()
 	{
 		transform.localEulerAngles = isLocked ? new Vector3(0, 0, 0) : new Vector3(0, 90, 0);
-        m_Game_Loop = GameObject.FindObjectOfType(typeof(Game_Loop)) as Game_Loop;
-        m_Game_Loop.TimetoPoints();
 	}
 }
