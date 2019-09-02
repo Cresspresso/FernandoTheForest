a63078b0603a0d14c00a2ab091fd2d41624e1881
1Angelika <53497909+1Angelika@users.noreply.github.com>
2019-09-02 16:11:46 +1200

    ...

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : Collectable {
+
+	public float wallHacksTime = 10;
+
+	public override void OnHeldBy(Player player)
+	{
+		base.OnHeldBy(player);
+		player.wallHacksTimer = wallHacksTime;
+	}
+}
---
+++ Assets/Scripts/Mushroom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : Collectable {
+
+	public float speedBonusTime = 10;
+	public ParticleSystem pickupEffects;
+
+	private void Start()
+	{
+		pickupEffects.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+		pickupEffects.gameObject.SetActive(false);
+	}
+
+	public override void OnHeldBy(Player player)
+	{
+		base.OnHeldBy(player);
+		player.GiveSpeedBoost(speedBonusTime);
+		pickupEffects.transform.SetParent(transform.parent);
+		pickupEffects.gameObject.SetActive(true);
+	}
+}
