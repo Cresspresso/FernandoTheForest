03f67c798617addf65292df47c449e811397d644
CSymphony <32253069+CSymphony@users.noreply.github.com>
2019-09-04 12:35:25 +1200

    Audio Scripts

--- FernandoTheForest/Assets/Scripts/Trophy.cs
+++ FernandoTheForest/Assets/Scripts/Trophy.cs
@@ -5,20 +5,14 @@ using UnityEngine;
 public class Trophy : Collectable {
 
 	public float points = 1000;
-
-    public AudioClip A_Trophy;
-    AudioSource S_Trophy;
-
-    private void Start()
-    {
-        S_Trophy = GetComponent<AudioSource>();
-    }
+    public AudioSource S_Trophy;
 
     public override void OnHeldBy(Player player)
 	{
-		base.OnHeldBy(player);
+        S_Trophy.Play(0);
+        player.points += points;
+        S_Trophy.transform.SetParent(null);
 
-        S_Trophy.PlayOneShot(A_Trophy, 1.0f);
-		player.points += points;
+        base.OnHeldBy(player);
 	}
 }
