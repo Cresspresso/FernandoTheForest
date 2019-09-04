67138b5033f28b493f3dcb566fd167cf5c359f32
CSymphony <32253069+CSymphony@users.noreply.github.com>
2019-09-04 15:43:10 +1200

    Adjustments

--- FernandoTheForest/Assets/Scripts/Trophy.cs
+++ FernandoTheForest/Assets/Scripts/Trophy.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trophy : Collectable {
+public class Trophy : TouchCollectable {
 
-	public float points = 1000;
+	public float points = 500;
     public AudioSource S_Trophy;
-
+    /*
     public override void OnHeldBy(Player player)
 	{
         S_Trophy.Play(0);
@@ -15,4 +15,19 @@ public class Trophy : Collectable {
 
         base.OnHeldBy(player);
 	}
+    */
+    protected override void OnTouched(Player player)
+    {
+        try
+        {
+            S_Trophy.Play(0);
+            S_Trophy.transform.SetParent(null);
+
+            player.points += points;
+        }
+        finally
+        {
+            base.OnTouched(player);
+        }
+    }
 }
