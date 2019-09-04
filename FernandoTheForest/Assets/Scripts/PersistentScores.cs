19d1a81188dd2772e1753302cbb66efb7701592f
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-09-04 17:01:43 +1200

    score board with scores

---
+++ FernandoTheForest/Assets/Scripts/PersistentScores.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentScores : MonoBehaviour {
+
+	public float[] scores = new float[4];
+
+	static PersistentScores instance_;
+	public static PersistentScores instance
+	{
+		get
+		{
+			if (!instance_)
+			{
+				instance_ = FindObjectOfType<PersistentScores>();
+			}
+			return instance_;
+		}
+		private set {
+			if (instance_)
+			{
+				Destroy(instance_);
+			}
+			instance_ = value;
+			DontDestroyOnLoad(value.gameObject);
+		}
+	}
+
+	private void Awake()
+	{
+		if (!instance_)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
